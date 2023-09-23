3bde8a2042462b0ae5dde20edf927138430bf0e0
Пользователь <@HOME-I5>
2023-09-23 11:57:35 +0700

    Добавление import'ов

--- M_Lab1_n2/M_Lab1_n2.py
+++ M_Lab1_n2/M_Lab1_n2.py
@@ -1,1 +1,9 @@
+import datetime
+import itertools    #итерации
 
+import matplotlib.pyplot as plt
+from matplotlib.dates import datestr2num, DateFormatter, DayLocator
+from matplotlib.ticker import AutoMinorLocator
+from matplotlib.patches import Patch
+import numpy as np
+import time         ##Время выполнения кода
