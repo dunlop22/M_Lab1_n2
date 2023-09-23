19b63f0740a19a99ec3d67bfc7b3b421ed6c4c9c
Пользователь <@HOME-I5>
2023-09-23 12:14:21 +0700

    Функция вывода матрицы

--- M_Lab1_n2/M_Lab1_n2.py
+++ M_Lab1_n2/M_Lab1_n2.py
@@ -9,7 +9,7 @@ import numpy as np
 import time         ##Время выполнения кода
 import os.path
 
-
+#Чтение данных из файла
 def read_file():
     if (os.path.exists("dannie.txt")):
         file1 = open("dannie.txt", "r")
@@ -21,28 +21,42 @@ def read_file():
             ##заполнение данными из файла матрицы
             arr = line.split()  #распределение строки из файла в массив
 
-            dannie.append([0]*len(arr))    #динамическое расширение массива данных
-            dannie1.append([0]*len(arr))   #динамическое расширение массива данных
-            dannie_copy.append([0]*len(arr))
+            dannie.append([0]*(len(arr) + 1))    #динамическое расширение массива данных
+            dannie1.append([0]*(len(arr) + 1))   #динамическое расширение массива данных
+            dannie_copy.append([0]*(len(arr) + 1))
 
             dannie[i][0] = i    #заполнение номера строки
-            for u in range(len(arr) - 1):
+            for u in range(len(arr)):
                 dannie[i][u + 1] = int(arr[u])
             dannie1[i] = dannie[i]      #Копирование исходных данных для дальнейшего восстановления последовательности деталей (по индексам)
             dannie_copy[i] = dannie[i]
             i = i + 1  
     
         num_det = i #Общее количество деталей
-        return True
+        return True #Успешное чтение данных из файла
     else:
         print("Ошибка открытия файла")
-        return False
+        return False #Чтение файла завершилось с ошибкой
+
+#Вывод матрицы
+def print_matrx(dannie_vivod):
+    for i in range(len(dannie_vivod)):
+        for j in range(len(dannie_vivod[i])):
+            print(dannie_vivod[i][j], end=" ")
+            if (j == 0):
+                print("| ", end="")
+        print() #Снос строки
+
+#Сортировка методом Джонсона
+def sort_Johnson():
+    ()
 
 dannie = []     #Исходные данные
 dannie_copy = []     #Исходные данные (для метода перебора)
-dannie1 = []    #SORT JOHNSON (A, B, C)
+dannie1 = []    #SORT JOHNSON (A, B)
 
 if (read_file()):
-    print("OK")
+    print_matrx(dannie)
+    print("OK")  
 
 
